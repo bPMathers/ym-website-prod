d754ecf8cea5c74c855355b1ee82c51ab8253fe0
Benjamin Proulx-Mathers <bpmathers@kinova.ca>
2026-02-14 19:51:37 -0500

    more chris bio update

--- src/Components/Team/ChrisPointFormComponent.js
+++ src/Components/Team/ChrisPointFormComponent.js
@@ -55,21 +55,22 @@ const ChrisPointFormComponent = () => {
                 <Typography variant="body2" className={classes.pointRowText}>
                     <span className={classes.point}>•</span>Music composer
                     within YUL MUSIC, where he composed, arranged and recorded
-                    music for the Cirque du Soleil, Moment Factory, The Montreal
-                    Canadiens, The Los Angeles Airport (LAX), The NBA, The Dubai
-                    Mall, Infinity, Google, Bulgari, as well as commercial
-                    jingles such as SC Johnson’s Glade North America television
-                    campaign and soundtracks for various shows, and more
-                    recently the opening soundtrack for the new Zac Brown Band’s
-                    Great American Road Trip Tour and Micheal Easton’s new short
-                    film, First Strike Butcher Knife.
+                    music for Mastercard, Cirque du Soleil, Moment Factory, The
+                    Montreal Canadiens, The Los Angeles Airport (LAX), The NBA,
+                    The FIFA, The Dubai Mall, Alibaba, Infinity, Skoda, Google,
+                    T-Mobile, Bulgari, Microsoft, Carnival Cruise Line, as well
+                    as commercial jingles such as SC Johnson's Glade North
+                    America television campaign and soundtracks for various
+                    shows, and more recently the opening soundtrack for the new
+                    Zac Brown Band’s Great American Road Trip Tour and Micheal
+                    Easton's new short film, First Strike Butcher Knife.
                 </Typography>
             </div>
             <div className={classes.pointRow}>
                 <Typography variant="body2" className={classes.pointRowText}>
                     <span className={classes.point}>•</span>Since 2000, Guy
-                    Laliberté’s musician for private and special events around
-                    the world.
+                    Laliberté’s (Cirque du Soleil) musician for private and
+                    special events around the world.
                 </Typography>
             </div>
             <div className={classes.pointRow}>
@@ -82,8 +83,8 @@ const ChrisPointFormComponent = () => {
             <div className={classes.pointRow}>
                 <Typography variant="body2" className={classes.pointRowText}>
                     <span className={classes.point}>•</span>Musical director,
-                    arranger, music composer and “remixer” for various
-                    large-scale special events for <i>Cirque du Soleil.</i>
+                    arranger, music composer and mixer for various large-scale
+                    special events for Cirque du Soleil.
                 </Typography>
             </div>
             <div className={classes.pointRow}>
@@ -91,16 +92,6 @@ const ChrisPointFormComponent = () => {
                     <span className={classes.point}>•</span>Responsible for the
                     recording studio located at Cirque du Soleil headquarters
                     during the summer seasons.
-                </Typography>
-            </div>
-            <div className={classes.pointRow}>
-                <Typography variant="body2" className={classes.pointRowText}>
-                    <span className={classes.point}>•</span>Permanent member of
-                    Drum Jungle, based in Los Angeles, with some of the best
-                    musicians in the industry from Los Angeles and New York,
-                    like Daniel de los Reyes (Sting), Horacio Hernandez
-                    (Santana), Luisito Quintero (Richard Bona), Ron Powell
-                    (Madonna) and Bashiri Johnson (Michael Jackson).
                 </Typography>
             </div>
             <div className={classes.pointRow}>
@@ -120,6 +111,23 @@ const ChrisPointFormComponent = () => {
             </div>
             <div className={classes.pointRow}>
                 <Typography variant="body2" className={classes.pointRowText}>
+                    <span className={classes.point}>•</span>Responsible for the
+                    recording studio located at Cirque du Soleil headquarters
+                    during the summer seasons.
+                </Typography>
+            </div>
+            <div className={classes.pointRow}>
+                <Typography variant="body2" className={classes.pointRowText}>
+                    <span className={classes.point}>•</span>Permanent member of
+                    Drum Jungle, based in Los Angeles, with some of the best
+                    musicians in the industry from Los Angeles and New York,
+                    like Daniel de los Reyes (Sting), Horacio Hernandez
+                    (Santana), Luisito Quintero (Richard Bona) and Ron Powell
+                    (Madonna).
+                </Typography>
+            </div>
+            <div className={classes.pointRow}>
+                <Typography variant="body2" className={classes.pointRowText}>
                     <span className={classes.point}>•</span>Several performances
                     at the NAMM Show in Los Angeles for sponsors such as DW
                     Drums, Latin Percussion, Sabian Cymbals, Regal Tip Sticks,
@@ -132,6 +140,24 @@ const ChrisPointFormComponent = () => {
                     <span className={classes.point}>•</span>Musician for
                     numerous world-renowned DJ’s: Louie Vega, Erik Morillo,
                     Behrouz, Roger Sanchez, David Morales.
+                </Typography>
+            </div>
+            <Typography variant="h2" className={classes.title}>
+                PROFESSIONAL EXPERIENCE ON THE LOCAL MUSIC SCENE
+            </Typography>
+            <div className={classes.pointRow}>
+                <Typography variant="body2" className={classes.pointRowText}>
+                    <span className={classes.point}>•</span>Accompanist or
+                    stand-in musician for TV stations house bands: Télé-Québec,
+                    Radio-Canada and TVA.
+                </Typography>
+            </div>
+            <div className={classes.pointRow}>
+                <Typography variant="body2" className={classes.pointRowText}>
+                    <span className={classes.point}>•</span>Accompanist for
+                    numerous artists on the Québec/France scene, such as
+                    Notre-Dame de Paris, Ariane Moffatt,&nbsp;Daniel Bélanger
+                    and Gilles Vigneault, with whom he worked for 12 years.
                 </Typography>
             </div>
         </div>
